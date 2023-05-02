be3d4ac27ff7f8cc543367159b30cbbed5da05c0
GetmevaDev <devteam@getmeva.com>
2023-05-03 01:02:57 +0600

    config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  env: {
-    API_URL: process.env.API_URL,
-    API_TOKEN: process.env.API_TOKEN,
-  },
   images: {
     domains: ["localhost", "res.cloudinary.com"],
     unoptimized: true,
