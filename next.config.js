6341f1bd63e73112ad5b34519a45287e66de272a
GetmevaDev <devteam@getmeva.com>
2023-05-03 00:55:46 +0600

    toml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  env: {
+    API_URL: process.env.API_URL,
+    API_TOKEN: process.env.API_TOKEN,
+  },
+  images: {
+    domains: ["localhost", "res.cloudinary.com"],
+    unoptimized: true,
+    disableStaticImages: true,
+  },
 };
 
 module.exports = nextConfig;
