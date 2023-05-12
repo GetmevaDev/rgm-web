7c3d9ec827c46a17230dbaaf09ee55e8507dd072
GetmevaDev <devteam@getmeva.com>
2023-05-12 14:07:49 +0600

    fix colors dark theme

--- src/shared/ui/Pledge/Pledge.jsx
+++ src/shared/ui/Pledge/Pledge.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+import { Typography } from "..";
+
 import styles from "./Pledge.module.scss";
 
-export const Pledge = () => <div className={styles.plegde}>plegde</div>;
+export const Pledge = () => (
+  <div className={styles.plegde}>
+    <Typography tag="h2" size="small">
+      MY PLEDGE TO YOU
+    </Typography>
+
+    <div className={styles.description}>
+      I am forthcoming as to what my SEO services can accomplish and am upfront
+      about my methods. For example, we will ONLY engage in white-hat tactics in
+      pursuing your goals, in accordance with guidelines setforth by the search
+      engines.
+    </div>
+  </div>
+);
