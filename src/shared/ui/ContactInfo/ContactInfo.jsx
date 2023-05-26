c6dd5d2337e2535db8e599863acefca305ce47e4
GetmevaDev <devteam@getmeva.com>
2023-05-26 15:24:31 +0600

    contact info

--- src/shared/ui/ContactInfo/ContactInfo.jsx
+++ src/shared/ui/ContactInfo/ContactInfo.jsx
@@ -1,23 +1,38 @@
 import React from "react";
+import { PulseLoader } from "react-spinners";
+import useSWR from "swr";
+
+import { fetcher, override } from "@/shared";
 
 import styles from "./ContactInfo.module.scss";
+//
 
-export const ContactInfo = () => (
-  <div className={styles.info}>
-    <h4 className={styles.title}>We are here!</h4>
+export const ContactInfo = () => {
+  const { data, isError, isLoading } = useSWR(
+    "https://cms-rgm.herokuapp.com/api/contact-info?populate=deep",
 
-    <div
-      className={styles.description}
-      dangerouslySetInnerHTML={{
-        __html: `
-					1501 Broadway, 12th Floor, 
-					New York, NY 10036 <br/>
-					<br/>
-					(888) 982-1410 <br/>
-					<br/>
-					info@robertgerov.com
-		`,
-      }}
-    />
-  </div>
-);
+    fetcher
+  );
+  if (isError) return <div>Error...</div>;
+
+  return (
+    <div className={styles.info}>
+      <h4 className={styles.title}>We are here!</h4>
+
+      {!isLoading ? (
+        <div className={styles.description}>
+          <span>{data?.data?.attributes?.address}</span>
+          <span>{data?.data?.attributes?.email}</span>
+          <span>{data?.data?.attributes?.phone}</span>
+        </div>
+      ) : (
+        <PulseLoader
+          color="#bda76a"
+          cssOverride={override}
+          size={10}
+          aria-label="Loading Spinner"
+        />
+      )}
+    </div>
+  );
+};
