dfe68953ad7f5b0992a38bd4f5f9209e26927382
GetmevaDev <75464863+GetmevaDev@users.noreply.github.com>
2024-11-04 11:31:17 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
 import { AnimatePresence, motion } from "framer-motion";
+import Head from 'next/head';
 import { useRouter } from "next/router";
 import Script from "next/script";
 import { ToastContainer } from "react-toastify";
@@ -35,15 +35,19 @@ export default function App({ Component, pageProps }) {
 
   return (
     <AnimatePresence mode="wait">
-    <Head>
-        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <Head>
+        <script>{"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KCWFCDNT');`}</script>
+        })(window,document,'script','dataLayer','GTM-KCWFCDNT');"}
+        </script>
       </Head>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCWFCDNT"
-                        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCWFCDNT"
+                        height="0" width="0" style="display:none;visibility:hidden">
+          </iframe>
+      </noscript>
       <motion.div
         key={router.route}
         initial="initialState"
