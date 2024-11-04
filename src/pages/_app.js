fc3252535f87f444b2ceb99111edc8ca46abc998
GetmevaDev <75464863+GetmevaDev@users.noreply.github.com>
2024-11-04 11:14:59 +0300

    Add Google Tag Manager to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 import Script from "next/script";
@@ -34,6 +35,15 @@ export default function App({ Component, pageProps }) {
 
   return (
     <AnimatePresence mode="wait">
+    <Head>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KCWFCDNT');</script>
+      </Head>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCWFCDNT"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <motion.div
         key={router.route}
         initial="initialState"
